9997ad3f2f771bbb5d18ffc97f7f206af8c03089
drtg46t34twetw34 <34135563+drtg46t34twetw34@users.noreply.github.com>
2019-07-15 00:42:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    const swearWords = ["ip","IP","اي بي"];
+    const swearWords = ["ip","IP"];
     if( swearWords.some(word => message.content.includes(word)) ) {
 
 	     message.react('👋');
